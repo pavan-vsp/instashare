b4399001cb81dd6359765005d51764e975999832
pavan-vsp <vempulurupavankumar@gmail.com>
2022-02-24 19:22:50 +0530

    src code

---
+++ src/ContextData/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const ContextData=React.createContext({
+  PostList:[],
+  SearchedPostList:()=>{},
+  SearchInput:'',
+  SearchInputChange:()=>{},
+  SearchValue:'',
+  ChangeSearchValue:()=>{},
+  TrigerSearchValue:()=>{},
+
+
+})
+
+export default ContextData
